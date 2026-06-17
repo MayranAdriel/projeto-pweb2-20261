b30db823bbdd0ac877f520d112459f8f9d20260f
MayranAdriel <mayranalbuquerque@gmail.com>
2026-06-17 00:48:56 -0300

    feat: ajustando config

---
+++ financas-api/src/main/java/br/edu/ifpb/financas/api/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package br.edu.ifpb.financas.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(cors -> {})
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
